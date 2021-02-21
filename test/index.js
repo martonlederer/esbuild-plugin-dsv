629625761119a6724ef31d8a6401607053045208
Marton Lederer <marton@lederer.hu>
2021-02-21 10:31:26 +0100

    chore: base

---
+++ test/index.js
@@ -0,0 +1,23 @@
+const { yamlPlugin } = require("../dist"),
+  { assert } = require("chai"),
+  { build } = require("esbuild");
+
+describe("DSV esbuild tests", () => {
+  it("Loads .tsv and .csv files", (done) => {
+    test("basic")
+      .then((res) => {
+        assert(res);
+        done();
+      })
+      .catch(done);
+  });
+});
+
+function test(test, options) {
+  return build({
+    entryPoints: [`tests/basic.ts`],
+    bundle: true,
+    outfile: `dist/${test}.js`,
+    plugins: [yamlPlugin(options)]
+  }).catch(() => process.exit(1));
+}
